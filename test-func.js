8ffcfd0e040e323a82009a3154bbe8c6dc4f7b36
cooper09 <cooper@sonyainc.net>
2019-10-29 11:44:59 -0400

    async working version with database POST

--- test-func.js
+++ test-func.js
@@ -1,9 +1,30 @@
 ( function( ) {
+    //cooper s - chaining async functions
 
     const func3 = async () => {
         console.log("func3..");
-        return "Func3 has the final say!";
-    }
+
+        var http = new XMLHttpRequest();
+        var url = 'https://intense-savannah-43793.herokuapp.com/track'
+
+        //var params = 'category='+newData.category+'&source='+newData.source+'&description='+newData.description+'&utm_campaign='+newData.utm_campaign+'&utm_source='+newData.utm_source+'&utm_medium='+newData.utm_medium+'&timestamp='+newData.timestamp;
+        var params = 'source=spinach&description=howdy&timestamp=now';
+        http.open('POST', url, true);
+
+        //Send the proper header information along with the request
+        http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+        http.onreadystatechange = function() {//Call a function when the state changes.
+            if(http.readyState == 4 && http.status == 200) {
+                window.location.href = 'http://sonyainc.net';
+                return "Watson I need you...";               
+            } else {
+                //alert("Failed - Update data: " + http.status);
+            }
+        }//end state change
+
+        http.send(params);
+    }//end func3
 
     const func2 = async () => {
         console.log("func2..");

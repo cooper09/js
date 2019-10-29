8055e38a35dce99a1c7232a4ae4d5419e5f3ae75
cooper09 <cooper@sonyainc.net>
2019-10-29 11:28:36 -0400

    now we'er groovin

--- test-func.js
+++ test-func.js
@@ -1,7 +1,34 @@
 ( function( ) {
-    console.log("Give me the func..");
 
- 
-    return "HERE we go!"
+    const func3 = async () => {
+        console.log("func3..");
+        return "Func3 has the final say!";
+    }
+
+    const func2 = async () => {
+        console.log("func2..");
+        return func3().then(
+            console.log("func3 has finished doing its thing...")
+        );
+    }
+
+    const func1 = async () => {
+        console.log("func1..");
+        return func2().then(
+            console.log("func2 has finished doing its thing...")
+        );
+    }
+
+    func1().then(result => {
+        console.log("And then....");
+        // process result here
+        document.getElementById("ad").innerHTML = result;
+      }).catch(err => {
+        // process error here
+        console.log("error: ", err );
+        document.getElementById("ad").innerHTML = "Shit!!!";
+      });
+  
+  
 })()
 
